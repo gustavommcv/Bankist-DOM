6908d9ca26ed075eea21543b35db58f067d1015f
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2024-10-01 21:35:46 -0300

    Implementing the open account menu

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
+// Header hamburguer menu
 const hamburguerMenu = document.querySelector('.header__button');
 const headerList = document.querySelector('.header__list');
 
 hamburguerMenu.addEventListener('click', () => {
     headerList.classList.toggle('header__list--active');
+});
+
+// Open account button
+const openAccountButton = document.querySelector('.header__link--open-acc');
+const openAccountMenu = document.querySelector('.open-account-menu');
+const blurContainer = document.querySelector('.blur-container');
+
+openAccountButton.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    openAccountMenu.classList.toggle('hidden');
+    blurContainer.classList.toggle('active');
+});
+
+blurContainer.addEventListener('click', (e) => {
+    // Clicking outside the menu to close it
+    e.preventDefault();
+
+    openAccountMenu.classList.toggle('hidden');
+    blurContainer.classList.toggle('active');
+});
+
+document.addEventListener('keydown', function(event) {
+    // Using escape key to exit the menu
+    if (event.key === 'Escape' && !openAccountMenu.classList.contains('hidden')) {
+        openAccountMenu.classList.toggle('hidden');
+        blurContainer.classList.toggle('active');
+    }
 });
