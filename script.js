1519a98a7a088a7cad8126089a42f23870ea8c9d
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2024-10-03 16:45:32 -0300

    Different ways of handling events and removing events

--- script.js
+++ script.js
@@ -48,4 +48,12 @@ btnScrollTo.addEventListener('click', (e) => {
 // LECTURES
 ///////////////////////////////////////////
 
+// const h1 = document.querySelector('h1');
 
+// const alertH1 = function (e) {
+//     alert('addEventListener: Great! You are reading the heading :D');
+
+//     h1.removeEventListener('mouseenter', alertH1);
+// }
+
+// h1.addEventListener('mouseenter', alertH1);
