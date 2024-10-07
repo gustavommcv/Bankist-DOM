625c16957071b142784668ea3fc853e43bba5b60
Gustavo Monnerat <monnerat.gustavo@outlook.com>
2024-10-07 19:33:34 -0300

    Finishing slider component

--- script.js
+++ script.js
@@ -223,6 +223,7 @@ const nextSlide = function() {
     }
 
     goToSlide(curSlide);
+    activateDot(curSlide);
 }
 btnRight.addEventListener('click', nextSlide);
 // -100% 0% 100% 200%
@@ -236,8 +237,39 @@ const prevSlide = function() {
     }
 
     goToSlide(curSlide);
+    activateDot(curSlide);
 }
 btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'ArrowLeft') prevSlide();
+    e.key === 'ArrowRight' && nextSlide();    
+})
+
+const dotContainer = document.querySelector('.dots');
+
+const createDots = function() {
+    slides.forEach(function(_, i) {
+        dotContainer.insertAdjacentHTML('beforeend', `
+            <button class="dots__dot" data-slide="${i}"></button>`);
+    });
+}
+createDots();
+
+const activateDot = function(slide) {
+    document.querySelectorAll('.dots__dot').forEach(dot => dot.classList.remove('dots__dot--active'));
+
+    document.querySelector(`.dots__dot[data-slide="${slide}"]`).classList.add('dots__dot--active');
+}
+activateDot(0);
+
+dotContainer.addEventListener('click', function(e) {
+    if(e.target.classList.contains('dots__dot')) {
+        const slide = e.target.dataset.slide;
+        goToSlide(slide);
+        activateDot(slide)
+    }
+});
 
 ///////////////////////////////////////////
 // LECTURES
